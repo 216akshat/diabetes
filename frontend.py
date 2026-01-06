0c56e41edfb9e62ee7c6c4c8a69885e8b1adbba8
Akshat Agarwal <akshatkumaragrwal@gmail.com>
2026-01-06 14:52:54 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -2,7 +2,6 @@ import streamlit as st
 import requests
 
 API_URL = "API_URL = "http://localhost:8000/predict"
-"
 
 st.set_page_config(page_title="Diabetes Probability Predictor", layout="centered")
 st.title("🩺 Diabetes Probability Predictor")
@@ -100,4 +99,5 @@ if st.button("Predict Diabetes Probability"):
             st.error(f"API Error: {response.text}")
     except Exception as e:
         st.error(f"Connection error: {e}")
+
 
